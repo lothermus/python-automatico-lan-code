19b0112f6d88050cbf87782ca6509a2e2dd01729
Washington Henrique <lothermus@gmail.com>
2026-05-19 16:43:27 -0300

    Dicionários em Python

---
+++ colecoes/dicionarios.py
@@ -0,0 +1,10 @@
+# O dicionário consegue armazenar uma série de informações, seja strings, valores, listas, etc.
+
+pessoa = {
+    'nome': 'Paulo',
+    'idade': 40,
+    'cidade': 'Divinópolis',
+    'profissao': ['Programador', 'Pedreiro', 'Mecânico']
+}
+
+print(pessoa['profissao'])
