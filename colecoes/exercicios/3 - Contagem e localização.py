e1c5d32a328dc5c538815a694ba98de7c5d55c5a
Washington Henrique <lothermus@gmail.com>
2026-05-19 16:08:58 -0300

    Novos exercícios

---
+++ colecoes/exercicios/3 - Contagem e localização.py
@@ -0,0 +1,15 @@
+# 🧪 Exercício 3 – Trabalhando com contagem e localização
+# Dada a lista abaixo:
+
+# nomes = [ "Ana", "Bruno", "Carla", "Daniel", "Eduarda", "Fernando", "Giovana", "Hugo", "Isabela", "João", "Carla", "Lucas", "Mariana", "Nuno", "Olivia", "João", "Pedro", "Carla", "Rafael", "Ana" ]
+
+# Mostre:
+
+# Quantas vezes o nome Carla aparece
+
+# Qual o índice da primeira vez que ele aparece
+
+nomes = [ "Ana", "Bruno", "Carla", "Daniel", "Eduarda", "Fernando", "Giovana", "Hugo", "Isabela", "João", "Carla", "Lucas", "Mariana", "Nuno", "Olivia", "João", "Pedro", "Carla", "Rafael", "Ana" ]
+
+print(f'O nome Carla aparece {nomes.count('Carla')} vezes dentro da lista e a primeira vez que aparece é no index {nomes.index('Carla')}!')
+
