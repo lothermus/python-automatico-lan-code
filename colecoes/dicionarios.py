9e6b38db35bd18a7e0a04cd57c6363602cbc5dc2
Washington Henrique <lothermus@gmail.com>
2026-05-19 17:52:09 -0300

    Update dicionarios.py

--- colecoes/dicionarios.py
+++ colecoes/dicionarios.py
@@ -31,4 +31,25 @@ print('------------')
 # Também é possível encapsular e transformar a key em uma lista, para que possamos citar os mesmos através do index:
 print('Encapsulando a chave para segmentar a mesma pela posição do index: ')
 valores = list(pessoa.values())
-print(valores[2])
+print(valores[1])
+print('------------')
+
+# Se utlizarmos o .items() conseguimos também citar as keys com os conteúdos das mesmas
+
+print('Encapsulamentos utilizando o .items()')
+print(pessoa.items())
+
+print('------------')
+
+# Se tentarmos buscar uma chave que não existe no dicionário, buscamos utilizando o .get()
+# E caso não tenha esse valor, será preenchido como "None" no console
+
+print(pessoa.get('telefone', 'Não existe')) # Dando uma " , " e passando uma string, o erro é informado como a string!
+print('------------')
+
+# Também é possível remover uma chave do dicionário utilizando o .pop()
+print('Removendo a key de "Profissões" utilizando o .pop(): ')
+item_removido = pessoa.pop('profissao')
+print(f'A key removida foi a {item_removido}')
+print('O restante das chaves é: ')
+print(pessoa)
