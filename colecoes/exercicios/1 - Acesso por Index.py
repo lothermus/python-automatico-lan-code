48033a9fb066e54869f8e3a4b4ba173fdca3d364
Washington Henrique <lothermus@gmail.com>
2026-05-19 14:05:12 -0300

    Novos exercícios

---
+++ colecoes/exercicios/1 - Acesso por Index.py
@@ -0,0 +1,6 @@
+# 🧪 Exercício 1 – Acesso por índice
+# Crie uma lista com 3 animais e mostre o primeiro e o último elemento.
+
+animais = ['leão', 'gorila', 'elefante']
+
+print(f'O primeiro animal da lista é: {animais[0].capitalize()} | E o último animal da lista é: {animais[-1].capitalize()}')
---
+++ colecoes/exercicios/2 - Manipulando listas.py
@@ -0,0 +1,35 @@
+# 🧪 Exercício 2 – Manipulando uma lista
+# Dada a lista abaixo:
+
+# livros = ["Python", "Java", "C++"]
+# Realize as seguintes ações:
+
+# Adicione o livro "JavaScript"
+
+# Remova o livro "Java"
+
+# Troque "Python" por "Go"
+
+# Mostre o tamanho da lista
+
+livros = ["Python", "Java", "C++"]
+
+print('Adicionando JavaScript na lista:')
+livros.append('JavaScript')
+print(livros)
+print('----------')
+
+print('Removendo o livro Java: ')
+print(f'O livro Java está na posição: {livros.index('Java')}')
+livros.pop(1)
+print('Agora a lista ficou com os livros restantes: ')
+print(livros)
+print('----------')
+
+print('Trocando "Python" for "Go": ')
+print(f'O livro Python está na posição {livros.index('Python')} da lista')
+livros[0] = 'Go'
+print(livros)
+print('----------')
+
+print(f'A lista tem um tamanho total de: {len((livros))} itens.')
