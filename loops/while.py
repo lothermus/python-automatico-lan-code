f1ccac5079737604fb223acab1214776ada9b0f4
Washington Henrique <lothermus@gmail.com>
2026-05-20 18:11:57 -0300

    Update while.py

--- loops/while.py
+++ loops/while.py
@@ -5,9 +5,23 @@
 #     contador += 1
 
 
-senha = ''
+# senha = '' # Cria uma variável vazia para receber o input
 
-while senha != 'Python':
-    senha = input('Digite sua senha: ')
+# while senha != 'Python': # Enquanto a senha for DIFERENTE do que está na variável
+#     senha = input('Digite sua senha: ') # Fica pedindo para digitar uma senha
 
-print ('Senha correta!')
+# print ('Senha correta!') # Quando digita a senha, o loop para!
+
+# for numero in range (6):
+#     print(numero)
+
+frutas = ['maçã', 'melancia', 'pessêgo', 'uva']
+
+for fruta in frutas:
+    print(f'Fruta atual: {fruta}')
+
+
+exo = [ 'membro 1', 'membro 2', 'membro 3']
+print(f'Os membros do Exo são: ')
+for membro in exo:
+    print(f'{membro}')
