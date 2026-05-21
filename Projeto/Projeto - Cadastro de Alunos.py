878f46341005f010a34094963a96ca926bd07cdf
Washington Henrique <lothermus@gmail.com>
2026-05-21 16:33:35 -0300

    Update Projeto - Cadastro de Alunos.py

--- Projeto/Projeto - Cadastro de Alunos.py
+++ Projeto/Projeto - Cadastro de Alunos.py
@@ -1,5 +1,12 @@
 
-while True: 
-    inicio = print(input('Seja bem-vindo! Selecione uma opção: \n1.Adicionar Aluno\n2.Listar todos os Alunos\n3.Buscar Aluno pelo nome\n4.Remover Aluno\n5.Mostrar média geral das notas\n6.Sair: '))
 
-    
+inicio = print(input('Seja bem-vindo! Selecione uma opção: \n1.Adicionar Aluno\n2.Listar todos os Alunos\n3.Buscar Aluno pelo nome\n4.Remover Aluno\n5.Mostrar média geral das notas\n6.Sair: '))
+
+nome_aluno = []
+
+match inicio:
+    case 1:
+        nome = input('Digite o nome do aluno: ')
+        nome_aluno = nome
+    case _:
+        
