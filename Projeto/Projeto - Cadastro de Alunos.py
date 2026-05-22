c7f9e7d3799d3cc3d1d34af1ec3c6aafee72d672
Washington Henrique <lothermus@gmail.com>
2026-05-22 11:10:10 -0300

    Update Projeto - Cadastro de Alunos.py

--- Projeto/Projeto - Cadastro de Alunos.py
+++ Projeto/Projeto - Cadastro de Alunos.py
@@ -1,12 +1,30 @@
 
+while True:
 
-inicio = print(input('Seja bem-vindo! Selecione uma opção: \n1.Adicionar Aluno\n2.Listar todos os Alunos\n3.Buscar Aluno pelo nome\n4.Remover Aluno\n5.Mostrar média geral das notas\n6.Sair: '))
+    inicio = int(input('Seja bem-vindo! Selecione uma opção: \n1.Adicionar Aluno\n2.Listar todos os Alunos\n3.Buscar Aluno pelo nome\n4.Remover Aluno\n5.Mostrar média geral das notas\n6.Sair: '))
 
-nome_aluno = []
+    aluno = {
+        'nome': '',
+        'idade': '',
+        'nota': ''
+    }
 
-match inicio:
-    case 1:
-        nome = input('Digite o nome do aluno: ')
-        nome_aluno = nome
-    case _:
-        
+    alunos = []
+
+    match inicio:
+        case 1:
+            nome_aluno = input('Digite o nome do aluno a ser adicionado: ')
+            aluno['nome'] = nome_aluno 
+            idade_aluno = input('Digite a idade do aluno: ')
+            aluno['idade'] = idade_aluno
+            nota_aluno = input('Digite a nota do aluno: ')
+            aluno['nota'] = nota_aluno
+            alunos.append(aluno)
+            print(f'O aluno foi adicionado. Seguem os dados:')
+            print(aluno)
+            aluno{}
+            print(alunos)
+        case 2:
+            print('Teste 2')
+        case _:
+            break
